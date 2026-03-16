7c51be376d1f6089934c02fa28b7489702c98586
David Coy <10887119+dcoy@users.noreply.github.com>
2026-03-16 20:34:23 +0000

    for_each functions

--- tf-code/main.tf
+++ tf-code/main.tf
@@ -1,19 +1,19 @@
-resource "random_id" "random" {
-  byte_length = 2
-  count       = var.repo_count
-}
+# resource "random_id" "random" {
+#   byte_length = 2
+#   count       = var.repo_count
+# }
 
 resource "github_repository" "tf_asct_repo" {
-  count       = var.repo_count
-  name        = "tf-asct-repo-${random_id.random[count.index].dec}"
-  description = "Code for tf associate"
+  for_each    = var.repos
+  name        = "tf-asct-repo-${each.key}"
+  description = "${each.value} code for tf associate"
   visibility  = var.env == "dev" ? "private" : "public"
   auto_init   = true
 }
 
 resource "github_repository_file" "readme" {
-  count               = var.repo_count
-  repository          = github_repository.tf_asct_repo[count.index].name
+  for_each            = var.repos
+  repository          = github_repository.tf_asct_repo[each.key].name
   branch              = "main"
   file                = "README.md"
   content             = "# Terraform Associate Certification - ${var.env}"
@@ -21,16 +21,16 @@ resource "github_repository_file" "readme" {
 }
 
 resource "github_repository_file" "index" {
-  count               = var.repo_count
-  repository          = github_repository.tf_asct_repo[count.index].name
+  for_each            = var.repos
+  repository          = github_repository.tf_asct_repo[each.key].name
   branch              = "main"
   file                = "index.html"
   content             = "Hello Terraform!"
   overwrite_on_create = true
 }
 
-output "repo-names" {
-  value       = { for i in github_repository.tf_asct_repo[*] : i.name => i.http_clone_url }
+output "clone-urls" {
+  value       = { for i in github_repository.tf_asct_repo : i.name => i.http_clone_url }
   description = "Repository names + URL"
   sensitive   = false
 }
--- tf-code/variables.tf
+++ tf-code/variables.tf
@@ -1,11 +1,11 @@
-variable "repo_count" {
+variable "repo_max" {
   type        = number
   description = "Number of repositories"
-  default     = 1
+  default     = 2
 
   validation {
-    condition     = var.repo_count < 5
-    error_message = "Don't deploy more than 5 repositories"
+    condition     = var.repo_max <= 10
+    error_message = "Don't deploy more than 10 repositories"
   }
 }
 
@@ -15,5 +15,14 @@ variable "env" {
   validation {
     condition     = contains(["dev", "prod"], var.env)
     error_message = "❌ Env must be dev or prod"
+  }
+}
+
+variable "repos" {
+  type        = set(string)
+  description = "Repository environment"
+  validation {
+    condition = length(var.repos) <= var.repo_max
+    error_message = "Max number of repos is greater than ${var.repo_max}"
   }
 }
